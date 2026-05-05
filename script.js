7bbea90660f25083c1408d7432481680a8eb39e1
Samy-y98 <sarthakchouksey48@gmail.com>
2026-05-05 23:54:29 +0530

    about section changed

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scrolled');
   }
 });
+
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  reveals.forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    const bottom = el.getBoundingClientRect().bottom;
+    const trigger = window.innerHeight * 0.85;
+
+    if (top < trigger && bottom > 0) {
+      el.classList.add('active');
+    } else {
+      el.classList.remove('active'); // 🔥 THIS was missing
+    }
+  });
+});
